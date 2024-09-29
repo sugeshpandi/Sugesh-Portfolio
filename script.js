c397bad6507786723cb5715f5f6eeb068d9eee05
sugeshpandi <sugeshpandisaravana@gmail.com>
2024-09-29 21:33:49 +0530

    Feat:new js

--- script.js
+++ script.js
@@ -9,4 +9,31 @@ document.addEventListener('DOMContentLoaded', function() {
     unorder.style.display = "block";
   }
 });
+const contactForm = document.querySelector('.contact-form');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const message = formData.get('message');
+
+  const params = {
+    from_name: name,
+    to_name: 'sugeshpandisaravana',
+    message: message,
+    reply_to: email,
+  };
+
+  const serviceID = 'default_service';
+  const templateID = 'template_12345';
+
+  emailjs.send(serviceID, templateID, params)
+    .then((res) => {
+      console.log('success', res.status);
+      alert('Message sent successfully!');
+    })
+    .catch((err) => console.error('Failed to send message', err));
+});
 });
